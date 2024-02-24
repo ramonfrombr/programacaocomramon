9e37e59f418f68dbafe2a4cd246d892123f0b57c
Ramon <ramonfrombr@gmail.com>
2024-02-24 19:06:12 -0300

    Installed shadcn-ui button. Included Button component in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-    return <p className="text-3xl font-medium text-sky-700">Hello world!</p>;
+    return (
+        <div>
+            <p className="text-3xl font-medium text-sky-700">Hello world!</p>
+            <Button variant="destructive">Click me</Button>
+        </div>
+    );
 }
