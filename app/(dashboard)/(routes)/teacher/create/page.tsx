4b717574fe304f9df284c7209750d31db235ce6a
Ramon <ramonfrombr@gmail.com>
2024-02-25 18:22:48 -0300

    Installed axios. Imported form, button, and input components from ui. Imported zodResolver. Imported useForm. imported useRouter

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,4 +1,20 @@
 "use client";
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/router";
+
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const CreatePage = () => {
     return <div>Create Course!</div>;
