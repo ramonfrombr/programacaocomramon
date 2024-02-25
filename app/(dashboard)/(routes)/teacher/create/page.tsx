b867d810c83e0aaa9ecd1d73e4885d3ad1a9aa46
Ramon <ramonfrombr@gmail.com>
2024-02-25 18:41:46 -0300

    Defined onSubmit function in CreatePage to make a request to api to create course. It returns a 404 error because the api endpoint is not implemented yet.

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -3,7 +3,7 @@ import * as z from "zod";
 import axios from "axios";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import {
     Form,
@@ -18,6 +18,7 @@ import {
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { Router } from "lucide-react";
 
 const formSchema = z.object({
     title: z.string().min(1, {
@@ -26,6 +27,8 @@ const formSchema = z.object({
 });
 
 const CreatePage = () => {
+    const router = useRouter();
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -35,8 +38,13 @@ const CreatePage = () => {
 
     const { isSubmitting, isValid } = form.formState;
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
-        console.log(values);
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await axios.post("/api/course", values);
+            router.push(`/teacher/courses/${response.data.id}`);
+        } catch {
+            console.log("Something went wrong");
+        }
     };
 
     return (
