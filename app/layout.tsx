15d58d8b3f82a6794f78dfc0f17d550f4d6f98d0
Ramon <ramonfrombr@gmail.com>
2024-02-25 18:46:37 -0300

    Included toast provider in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ptBR } from "@clerk/localizations";
+import { ToastProvider } from "@/components/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,8 @@ export default function RootLayout({
         <ClerkProvider localization={ptBR}>
             <html lang="en">
                 <body className={inter.className}>{children}</body>
+                <ToastProvider />
+                {children}
             </html>
         </ClerkProvider>
     );
