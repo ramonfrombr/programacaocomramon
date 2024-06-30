4b025df4b57322eccfeb9feda34d4ef235a49d4c
Ramon <ramonfrombr@gmail.com>
2024-06-30 19:47:54 -0300

    Created title-form component and included in course page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -3,6 +3,7 @@ import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
+import { TitleForm } from "./_components/title-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth();
@@ -25,6 +26,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
         course.title,
         course.description,
         course.imageUrl,
+        course.price,
         course.categoryId,
     ];
 
@@ -51,6 +53,7 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                         <IconBadge icon={LayoutDashboard} />
                         <h2 className="text-xl">Customize your course</h2>
                     </div>
+                    <TitleForm initialData={course} courseId={course.id} />
                 </div>
             </div>
         </div>
