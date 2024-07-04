027e12b24b696db6f77351179792be211f410d6d
Ramon <ramonfrombr@gmail.com>
2024-07-04 18:12:12 -0300

    Remove middlewares

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    // Protects all routes, including api/trpc.
-    // See https://clerk.com/docs/references/nextjs/auth-middleware
-    // for more information about configuring your Middleware
-    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
