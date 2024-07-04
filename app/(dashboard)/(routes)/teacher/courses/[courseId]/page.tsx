402984838dc51020db0ec1f61a66d96377d54c50
Ramon <ramonfrombr@gmail.com>
2024-07-04 03:42:44 -0300

    Created price-form component and included in course page

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -7,6 +7,7 @@ import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
 import { CategoryForm } from "./_components/category-form";
+import { PriceForm } from "./_components/price-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
     const { userId } = auth();
@@ -82,13 +83,14 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
                             <IconBadge icon={ListChecks} />
                             <h2 className="text-xl">Course chapters</h2>
                         </div>
+                        <div>TODO: Chapters</div>
                     </div>
-                    <div>TODO: Chapters</div>
-                </div>
-                <div>
-                    <div className="flex items-center gap-x-2">
-                        <IconBadge icon={CircleDollarSign} />
-                        <h2 className="text-xl">Sell your course</h2>
+                    <div>
+                        <div className="flex items-center gap-x-2">
+                            <IconBadge icon={CircleDollarSign} />
+                            <h2 className="text-xl">Sell your course</h2>
+                        </div>
+                        <PriceForm initialData={course} courseId={course.id} />
                     </div>
                 </div>
             </div>
