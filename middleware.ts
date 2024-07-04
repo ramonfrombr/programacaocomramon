36264198835a920a27788d168087316f03674d73
Ramon <ramonfrombr@gmail.com>
2024-07-04 18:08:53 -0300

    Removed auth middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,3 @@
-import { authMiddleware } from "@clerk/nextjs";
-
-export default authMiddleware({ publicRoutes: ["/api/uploadthing"] });
-
 export const config = {
     // Protects all routes, including api/trpc.
     // See https://clerk.com/docs/references/nextjs/auth-middleware
