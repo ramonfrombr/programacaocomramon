83937bb884a5ff82d87a51f10678f276789e1b48
ramonfrombr <ramonfrombr@gmail.com>
2024-07-09 17:30:11 -0300

    Display real courses information in data table instead of dummy data

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,24 +1,28 @@
+import { auth } from "@clerk/nextjs";
 import { columns } from "./_components/columns";
 import { DataTable } from "./_components/data-table";
-
-async function getData(): Promise<any[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ];
-}
+import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
 
 const CoursesPage = async () => {
-  const data = await getData();
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <DataTable columns={columns} data={data} />
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
