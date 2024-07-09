f1aca5fccd18f7a65a01e7d2157cc40c46245062
ramonfrombr <ramonfrombr@gmail.com>
2024-07-09 17:25:38 -0300

    Installed shadcn ui data table and displayed dummy table in teacher courses page

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,26 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { columns } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
 
-const CoursesPage = () => {
-    return (
-        <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>New Course</Button>
-            </Link>
-        </div>
-    );
+async function getData(): Promise<any[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+const CoursesPage = async () => {
+  const data = await getData();
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 };
 
 export default CoursesPage;
