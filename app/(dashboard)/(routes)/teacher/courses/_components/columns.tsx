90fe166dbe656b675a4d17043df23de2d6fe2cf3
ramonfrombr <ramonfrombr@gmail.com>
2024-07-09 18:02:15 -0300

    Price column in courses data table now displays formatted price

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -43,6 +43,15 @@ export const columns: ColumnDef<Course>[] = [
         </Button>
       );
     },
+    cell: ({ row }) => {
+      const price = parseFloat(row.getValue("price") || "0");
+      const formatted = new Intl.NumberFormat("EN-us", {
+        style: "currency",
+        currency: "USD",
+      }).format(price);
+
+      return <div>{formatted}</div>;
+    },
   },
   {
     accessorKey: "isPublished",
