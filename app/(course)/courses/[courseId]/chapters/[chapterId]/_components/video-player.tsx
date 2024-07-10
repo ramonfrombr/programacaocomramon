5272a5666831da84fc1247d18fdcf7cd54a2850a
ramonfrombr <ramonfrombr@gmail.com>
2024-07-10 10:35:13 -0300

    Chapter page displays placeholder for locked and available chapters

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -28,5 +28,19 @@ export const VideoPlayer = ({
   completeOnEnd,
   title,
 }: VideoPlayerProps) => {
-  return <div>Video Player</div>;
+  return (
+    <div className="relative aspect-video">
+      {!isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
+          <Loader2 className="h-8 w-8 animate-spin text-secondary" />
+        </div>
+      )}
+      {isLocked && (
+        <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+          <Lock className="w-8 h-8" />
+          <p className="text-sm">This chapter is locked</p>
+        </div>
+      )}
+    </div>
+  );
 };
