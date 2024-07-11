5ace284f4289694609fc571b155281976ad594de
ramonfrombr <ramonfrombr@gmail.com>
2024-07-11 17:34:43 -0300

    Displayed analytics chart in analytics page

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { getAnalytics } from "@/actions/get-analytics";
 import { DataCard } from "./_components/data-card";
+import { Chart } from "./_components/chart";
 
 const AnalyticsPage = async () => {
   const { userId } = auth();
@@ -15,9 +16,10 @@ const AnalyticsPage = async () => {
     <div className="p-6">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
         <DataCard value={totalRevenue} label="Total Revenue" shouldFormat />
-
         <DataCard value={totalSales} label="Total Sales" />
       </div>
+
+      <Chart data={data} />
     </div>
   );
 };
