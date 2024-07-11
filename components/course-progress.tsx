902b4088c25ca4ccb2cc05ea2077d5666c8b054a
ramonfrombr <ramonfrombr@gmail.com>
2024-07-11 14:09:41 -0300

    Created course-progress component and added to course-sidebar component

---
+++ components/course-progress.tsx
@@ -0,0 +1,29 @@
+import { Progress } from "@/components/ui/progress";
+
+interface CourseProgressProps {
+  value: number;
+  variant?: "default" | "success";
+  size?: "default" | "sm";
+}
+
+const colorByVariant = {
+  default: "text-sky-700",
+  success: "text-emerald-700",
+};
+
+const sizeByVariant = {
+  default: "text-sm",
+  success: "text-xs",
+};
+
+export const CourseProgress = ({
+  value,
+  variant,
+  size,
+}: CourseProgressProps) => {
+  return (
+    <div>
+      <Progress variant={variant} className="h-2" value={value} />
+    </div>
+  );
+};
