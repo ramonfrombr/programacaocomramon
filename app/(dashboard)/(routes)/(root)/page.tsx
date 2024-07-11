2b1dce34760ed0e9ca9dd4e98a0ae773353ec5d3
ramonfrombr <ramonfrombr@gmail.com>
2024-07-11 16:26:06 -0300

    Display list of enrolled courses in student dashboard

---
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -0,0 +1,27 @@
+import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+export default async function Dashboard() {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const { completedCourses, coursesInProgress } = await getDashboardCourses(
+    userId
+  );
+
+  return (
+    <div className="p-6 space-y-4">
+      <div className="grid grid-cols-1 dm:grid-cols-2 gap-4">
+        <div>{/* TODO: Info Card */}</div>
+        <div>{/* TODO: Info Card */}</div>
+
+        <CoursesList items={[...coursesInProgress, ...completedCourses]} />
+      </div>
+    </div>
+  );
+}
