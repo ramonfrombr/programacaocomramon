b2640d5415613bc28dc2f5466b80200202966413
ramonfrombr <ramonfrombr@gmail.com>
2024-07-11 17:47:16 -0300

    Conditionally renders teacher mode button based on whether the user is a teacher

--- components/navbar-routes.tsx
+++ components/navbar-routes.tsx
@@ -1,12 +1,14 @@
 "use client";
-import { UserButton } from "@clerk/nextjs";
+import { useAuth, UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 import { Button } from "./ui/button";
 import { LogOut } from "lucide-react";
 import Link from "next/link";
 import { SearchInput } from "./search-input";
+import { isTeacher } from "@/lib/teacher";
 
 export const NavbarRoutes = () => {
+  const { userId } = useAuth();
   const pathname = usePathname();
   const isTeacherPage = pathname?.startsWith("/teacher");
   const isCoursePage = pathname?.includes("/courses");
@@ -26,13 +28,13 @@ export const NavbarRoutes = () => {
               <LogOut className="w-4 h-4 mr-2" /> Exit
             </Button>
           </Link>
-        ) : (
+        ) : isTeacher(userId) ? (
           <Link href="/teacher/courses">
             <Button size="sm" variant="ghost">
               Teacher mode
             </Button>
           </Link>
-        )}
+        ) : null}
         <UserButton afterSignOutUrl="/" />
       </div>
     </>
