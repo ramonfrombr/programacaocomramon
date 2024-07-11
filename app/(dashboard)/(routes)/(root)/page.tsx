a56a63552367640293d89520d0e04f553ce83878
ramonfrombr <ramonfrombr@gmail.com>
2024-07-11 16:35:23 -0300

    Added InfoCards to dashboard page

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -1,7 +1,9 @@
 import { getDashboardCourses } from "@/actions/get-dashboard-courses";
 import { CoursesList } from "@/components/courses-list";
 import { auth } from "@clerk/nextjs";
+import { CheckCircle, Clock } from "lucide-react";
 import { redirect } from "next/navigation";
+import { InfoCard } from "./_components/info-card";
 
 export default async function Dashboard() {
   const { userId } = auth();
@@ -16,12 +18,20 @@ export default async function Dashboard() {
 
   return (
     <div className="p-6 space-y-4">
-      <div className="grid grid-cols-1 dm:grid-cols-2 gap-4">
-        <div>{/* TODO: Info Card */}</div>
-        <div>{/* TODO: Info Card */}</div>
-
-        <CoursesList items={[...coursesInProgress, ...completedCourses]} />
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        <InfoCard
+          icon={Clock}
+          label="In Progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="Completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+        />
       </div>
+      <CoursesList items={[...coursesInProgress, ...completedCourses]} />
     </div>
   );
 }
