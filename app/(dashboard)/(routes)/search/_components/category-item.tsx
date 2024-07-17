b6f7e9e1014eda0919f802032f07af0f61286827
ramonfrombr <ramonfrombr@gmail.com>
2024-07-17 20:15:00 -0300

    Removed icon from category-item component

--- app/(dashboard)/(routes)/search/_components/category-item.tsx
+++ app/(dashboard)/(routes)/search/_components/category-item.tsx
@@ -3,19 +3,13 @@
 import qs from "query-string";
 import { cn } from "@/lib/utils";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { IconType } from "react-icons/lib";
 
 interface CategoryItemProps {
   label: string;
   value?: string;
-  icon?: IconType;
 }
 
-export const CategoryItem = ({
-  label,
-  value,
-  icon: Icon,
-}: CategoryItemProps) => {
+export const CategoryItem = ({ label, value }: CategoryItemProps) => {
   const pathname = usePathname();
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -44,12 +38,12 @@ export const CategoryItem = ({
     <button
       onClick={onClick}
       className={cn(
-        "py-2 px-3 text-sm border border-slate-200 rounded-full flex items-center gap-x-1 hover:border-sky-700 transition",
-        isSelected && "border-sky-700 bg-sky-200/20 text-sky-800"
+        "px-2 py-1 text-sm font-semibold border border-slate-200 rounded flex items-center gap-x-1 transition bg-white",
+        isSelected && "border-sky-700 bg-sky-100 text-sky-800"
       )}
       type="button"
     >
-      {Icon && <Icon size={20} />} <div className="truncate">{label}</div>
+      <div className="truncate">{label}</div>
     </button>
   );
 };
