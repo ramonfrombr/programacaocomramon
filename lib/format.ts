1194ac25ecb609015cf4b1a4165bc6004bea296f
ramonfrombr <ramonfrombr@gmail.com>
2024-07-17 20:13:33 -0300

    Added multi currency options to format currency function

--- lib/format.ts
+++ lib/format.ts
@@ -1,6 +1,32 @@
+let currency;
+let languageFormat;
+
+switch (process.env.NEXT_PUBLIC_LANGUAGE) {
+  case "portuguese":
+    currency = "BRL";
+    languageFormat = "pt-BR";
+    break;
+  case "spanish":
+    currency = "USD";
+    languageFormat = "es-ES";
+    break;
+  case "french":
+    currency = "EUR";
+    languageFormat = "fr-FR";
+    break;
+  case "english":
+    currency = "USD";
+    languageFormat = "en-US";
+    break;
+  default:
+    currency = "USD";
+    languageFormat = "en-US";
+    break;
+}
+
 export const formatPrice = (price: number) => {
-    return new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-    }).format(price);
+  return new Intl.NumberFormat(languageFormat, {
+    style: "currency",
+    currency: currency,
+  }).format(price);
 };
