d1798cd1ad86aa062d3f2eea3e86427eb5ae442b
ramonfrombr <ramonfrombr@gmail.com>
2024-07-20 21:54:29 -0300

    Updated clerk/nextjs version

--- app/api/courses/[courseId]/checkout/route.ts
+++ app/api/courses/[courseId]/checkout/route.ts
@@ -1,6 +1,6 @@
 import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
-import { currentUser } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
