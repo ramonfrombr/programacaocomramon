49665f264cb9bb64f3b745fb5fefc513bd7ff91c
ramonfrombr <ramonfrombr@gmail.com>
2024-08-19 21:43:14 -0300

    Added footer to dashboard layout. Added redirect if not loggedin to dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Sidebar } from "./_components/sidebar";
 import { Navbar } from "./_components/Navbar";
+import { Footer } from "@/components/footer-dashboard";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
   return (
     <div className="h-full">
       <div className="h-[80px] md:pl-[300px] fixed inset-y-0 w-full z-50">
@@ -11,8 +20,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="hidden md:flex w-56 flex-col fixed inset-y-0 z-50">
         <Sidebar />
       </div>
-      <main className="md:pl-[300px] pt-[80px] min-h-full bg-gray-50">
+      <main className="md:pl-[300px] pt-[80px] min-h-screen bg-gray-50 flex flex-col justify-between">
         {children}
+        <Footer />
       </main>
     </div>
   );
