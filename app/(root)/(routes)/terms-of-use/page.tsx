8019c296c03fe3ad8f17e5ef4e1a2ab684f06dce
ramonfrombr <ramonfrombr@gmail.com>
2024-08-28 04:52:15 -0300

    Created Terms of Use page

---
+++ app/(root)/(routes)/terms-of-use/page.tsx
@@ -0,0 +1,223 @@
+import React from "react";
+
+const TermsOfUsePage = () => {
+  return (
+    <div className="p-8 md:px-32 lg:px-52 [&_p]:mb-5 [&_h2]:mb-3 [&_h2]:text-2xl [&_h2]:font-semibold [&_p]:text-sm">
+      <h1 className="font-semibold text-3xl mb-3">Termos e Condições</h1>
+      <p>Última atualização: 28 de agosto de 2024</p>
+      <p>
+        Estes Termos e Condições (&quot;Termos&quot;) regem seu relacionamento
+        com o site e a aplicação web da Programação com Ramon (o
+        &quot;Serviço&quot;), disponível em https://programacaocomramon.com.
+        Estes Termos se aplicam a todos os visitantes, usuários e outros que
+        acessam ou utilizam o Serviço, incluindo aqueles que utilizam o Serviço
+        por meio de acesso gratuito, compra única ou assinatura.
+      </p>
+      <h2>Aceitação dos Termos</h2>
+      <p>
+        Ao acessar ou utilizar o Serviço, você concorda em estar vinculado a
+        estes Termos e aceita todas as consequências legais. Se você não
+        concordar com estes Termos, por favor, não utilize o Serviço.
+      </p>
+      <h2>Termos Suplementares</h2>
+      <p>
+        Ao utilizar o Cliente da API do Programação com Ramon no YouTube, você
+        também concorda com os Termos de Serviço do YouTube, disponíveis em:
+        https://www.youtube.com/t/terms.
+      </p>
+      <h2>Conteúdo</h2>
+      <p>
+        Você é responsável pela legalidade, confiabilidade e adequação de
+        qualquer informação, texto, gráficos, vídeos ou outro material
+        (&quot;Conteúdo&quot;) que você publique no Serviço. Ao publicar
+        Conteúdo, você nos concede o direito de usar, modificar e distribuir seu
+        Conteúdo como parte do Serviço. Você afirma que possui ou tem as
+        licenças necessárias para o Conteúdo e que ele não infringe nenhum
+        direito.
+      </p>
+      <h2>Contas</h2>
+      <p>
+        Você deve fornecer informações precisas e atualizadas ao criar sua
+        conta. Você é responsável por proteger sua senha e por quaisquer
+        atividades realizadas sob sua conta. Notifique-nos imediatamente sobre
+        qualquer uso não autorizado de sua conta.
+      </p>
+      <h2>Atividades Proibidas</h2>
+      <p>
+        O download, cópia, reprodução, redistribuição ou qualquer outro uso não
+        autorizado do material e conteúdo fornecido em nosso Serviço sem
+        permissão expressa por escrito é estritamente proibido. Todo o conteúdo
+        e material disponível através do Serviço, incluindo, mas não se
+        limitando a texto, gráficos, logotipos, vídeos, informações e software,
+        são propriedade exclusiva do Programação com Ramon ou de seus
+        licenciadores.
+      </p>
+      <p>
+        Os usuários recebem uma licença limitada, não exclusiva e intransferível
+        para acessar e utilizar o Serviço e seu conteúdo para fins pessoais e
+        não comerciais, conforme pretendido pela funcionalidade fornecida pelo
+        Serviço e sob estes Termos. Qualquer uso do Serviço ou seu conteúdo,
+        além do especificamente autorizado aqui, sem permissão prévia por
+        escrito, constitui uma violação destes Termos e pode resultar em ação
+        legal, incluindo, mas não se limitando a penalidades civis e criminais.
+      </p>
+      <p>
+        Se for constatado que você está na posse não autorizada ou distribuindo
+        conteúdo do Serviço, sua conta poderá ser suspensa ou encerrada, e você
+        poderá estar sujeito a multas e ações legais por violação de direitos
+        autorais.
+      </p>
+      <h2>Conduta e Padrões de Conteúdo dos Usuários</h2>
+      <p>
+        Estamos comprometidos em manter uma comunidade respeitosa em nosso
+        Serviço. Espera-se que os usuários se comportem de maneira civil e
+        respeitosa com os outros. Comportamento inadequado, incluindo o uso de
+        palavrões, linguagem ofensiva ou outras formas de comunicação que possam
+        ser consideradas assédio ou depreciativas para os outros, é estritamente
+        proibido.
+      </p>
+      <p>
+        Nossa plataforma oferece funcionalidade limitada para os usuários
+        postarem perguntas e outros conteúdos que podem ser visíveis para outros
+        usuários. É importante que todos os usuários aderem a esses padrões de
+        conteúdo para garantir um ambiente positivo e produtivo para todos.
+        Qualquer conteúdo postado por usuários deve cumprir com esses padrões e
+        todas as leis aplicáveis.
+      </p>
+      <p>
+        Reservamo-nos o direito de revisar e moderar o conteúdo postado pelos
+        usuários. Se o conteúdo for encontrado em violação de nossas políticas,
+        podemos removê-lo, restringir a capacidade do usuário de postar mais
+        conteúdo e/ou suspender ou encerrar a conta do usuário, dependendo da
+        gravidade da violação.
+      </p>
+      <p>
+        Os usuários são incentivados a relatar qualquer comportamento ou
+        conteúdo que viole esses padrões através dos canais apropriados
+        fornecidos pelo Serviço. Estamos comprometidos em resolver problemas de
+        forma rápida e justa.
+      </p>
+      <h2>Assinaturas Mensais</h2>
+      <p>
+        Nosso Serviço oferece opções de assinatura mensal que concedem acesso
+        contínuo aos recursos do Serviço até que seja cancelado pelo usuário ou
+        encerrado devido a falta de pagamento. Se você optar por assinar,
+        concorda com a cobrança automática mensal da taxa de assinatura no
+        método de pagamento fornecido.
+      </p>
+      <p>
+        Se você cancelar sua assinatura, seu acesso aos recursos baseados em
+        assinatura continuará até o final do ciclo de faturamento atual. O
+        cancelamento não requer um reembolso proporcional; em vez disso, o
+        acesso completo continua até o final do ciclo de faturamento.
+      </p>
+      <p>
+        Se um pagamento falhar, sua assinatura será suspensa e o acesso aos
+        serviços de assinatura será encerrado.
+      </p>
+      <p>
+        O término da sua assinatura por qualquer motivo requer uma nova
+        assinatura para recuperar o acesso e estará sujeito à tarifa de
+        assinatura vigente no momento da nova assinatura. Reservamo-nos o
+        direito de alterar os preços das assinaturas. Quaisquer mudanças de
+        preço serão comunicadas aos assinantes atuais com pelo menos 30 dias de
+        antecedência antes da nova tarifa entrar em vigor.
+      </p>
+      <h2>Atualizações de Conteúdo e Disponibilidade</h2>
+      <p>
+        Embora nos esforcemos para manter o conteúdo em nosso Serviço atualizado
+        e preciso, não podemos garantir que todas as informações disponíveis
+        através do Serviço reflitam o conhecimento ou desenvolvimentos mais
+        atuais. O conteúdo pode se tornar desatualizado com o tempo, e podemos
+        ou não atualizar o conteúdo desatualizado com base em nosso critério e
+        recursos disponíveis.
+      </p>
+      <p>
+        Atualizações ao conteúdo existente ou a liberação de novo conteúdo não
+        são garantidas para ocorrer em um cronograma regular e podem ocorrer a
+        qualquer momento. Reservamo-nos o direito de determinar a frequência e a
+        extensão das atualizações do Serviço.
+      </p>
+      <p>
+        Além disso, certos conteúdos ou recursos podem ser oferecidos sob
+        &quot;Acesso Antecipado Privado&quot;, que não está incluído como parte
+        da compra padrão ou de assinatura. O acesso ao conteúdo de Acesso
+        Antecipado Privado requer elegibilidade separada e pode envolver taxas
+        adicionais. Esses conteúdos são claramente marcados e diferenciados das
+        ofertas regulares para garantir transparência.
+      </p>
+      <h2>Política de Reembolso</h2>
+      <p>
+        Reembolsos para compras únicas ou taxas de assinatura são concedidos a
+        critério exclusivo de nossa plataforma. Para solicitar um reembolso,
+        você deve entrar em contato com nossa equipe de suporte ao cliente com
+        os detalhes do seu pedido e o motivo da solicitação de reembolso dentro
+        de 30 dias após a compra ou renovação da assinatura.
+      </p>
+      <p>
+        Avaliaremos os pedidos de reembolso caso a caso, considerando fatores
+        como o uso do Serviço, a natureza do conteúdo acessado e os motivos
+        fornecidos para insatisfação. Nossa decisão sobre reembolsos considerará
+        a justiça para o usuário e a plataforma, e buscamos resolver tais
+        pedidos de maneira amigável.
+      </p>
+      <p>
+        Reembolsos, se aprovados, serão processados para o método de pagamento
+        original utilizado no momento da compra. Observe que certos conteúdos ou
+        serviços, particularmente aqueles envolvendo ofertas de terceiros ou
+        bens digitais, podem não ser reembolsáveis. Incentivamos os usuários a
+        ler os termos específicos relacionados a cada compra.
+      </p>
+      <h2>Propriedade Intelectual</h2>
+      <p>
+        Respeitamos os direitos de propriedade intelectual dos outros e
+        esperamos que os usuários façam o mesmo. Reclamações de violação de
+        direitos autorais devem ser enviadas por escrito para
+        programacaocomramon@gmail.com, incluindo uma descrição detalhada da
+        suposta violação.
+      </p>
+      <h2>Links para Outros Sites</h2>
+      <p>
+        Nosso Serviço pode incluir links para sites de terceiros. O Programação
+        com Ramon não é responsável pelo conteúdo ou pelas práticas desses
+        sites. Encorajamos você a revisar seus termos e condições.
+      </p>
+      <h2>Rescisão e Suspensão</h2>
+      <p>
+        Podemos rescindir ou suspender seu acesso ao nosso Serviço
+        imediatamente, sem aviso prévio, por qualquer violação destes Termos.
+        Após a rescisão, seu direito de usar o Serviço cessa.
+      </p>
+      <h2>Indemnização</h2>
+      <p>
+        Você concorda em indenizar o Programação com Ramon contra quaisquer
+        reivindicações, perdas ou danos decorrentes do uso do Serviço ou da
+        violação destes Termos.
+      </p>
+      <h2>Limitação de Responsabilidade</h2>
+      <p>
+        O Programação com Ramon não é responsável por quaisquer perdas ou danos
+        diretos ou indiretos decorrentes do uso do Serviço ou do acesso não
+        autorizado ao seu conteúdo.
+      </p>
+      <h2>Isenção de Responsabilidade</h2>
+      <p>
+        O Serviço é fornecido &quot;NO ESTADO EM QUE SE ENCONTRA&quot;, sem
+        garantias de qualquer tipo. Não garantimos acesso ininterrupto ou a
+        correção de defeitos.
+      </p>
+      <h2>Alterações nos Termos</h2>
+      <p>
+        Reservamo-nos o direito de modificar estes Termos a qualquer momento.
+        Forneceremos aviso sobre alterações materiais. O uso contínuo do Serviço
+        após as alterações constitui aceitação dos novos termos.
+      </p>
+      <h2>Legislação Aplicável</h2>
+      <p>Estes Termos são regidos pelas leis do Brasil.</p>
+      <h2>Entre em Contato Conosco</h2>
+      <p>Para dúvidas sobre estes Termos, entre em contato com nossa equipe.</p>
+    </div>
+  );
+};
+
+export default TermsOfUsePage;
